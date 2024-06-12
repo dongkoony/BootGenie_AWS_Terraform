f94a8e0b2264b7ff48bba587ae4891087f2d5696
donghyeonshin <donghyun4591@gmail.com>
2024-06-12 13:22:04 +0900

    -

--- script/jenkins_container.sh
+++ script/jenkins_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# SSH 포트 변경
+sed -i 's/#Port 22/Port 1717/g' /etc/ssh/sshd_config
+
+# SSH 서비스 재시작
+systemctl restart sshd
+
+# 방화벽 설정 (필요한 경우)
+ufw allow 1717/tcp
+
+
 # Docker 설치
 apt-get update
 apt-get install -y ca-certificates curl gnupg lsb-release
