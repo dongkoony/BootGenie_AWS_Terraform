8cfb47ee535b92aace3f375d3c427333e6ac42e7
donghyeonshin <donghyun4591@gmail.com>
2024-07-06 11:41:15 +0900

    asg outputs.tf 수정.

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,38 +1,17 @@
 # ./modules/ec2/outputs.tf
 
-# output "app_instance_ids" {
-#   description = "IDs of the App instances"
-#   value       = [for instance in aws_instance.app : instance.id]
-# }
-
-# output "web_instance_ids" {
-#   description = "IDs of the Web instances"
-#   value       = [for instance in aws_instance.web : instance.id]
-# }
-
-# output "app_instance_public_ips" {
-#   description = "Public IPs of the App instances"
-#   value       = [for instance in aws_instance.app : instance.public_ip]
-# }
-
-# output "web_instance_public_ips" {
-#   description = "Public IPs of the Web instances"
-#   value       = [for instance in aws_instance.web : instance.public_ip]
-# }
-
-
-output "app_instance_ids" {
-  value = aws_instance.app[*].id
+output "app_launch_template_id" {
+  value = aws_launch_template.app.id
 }
 
-output "web_instance_ids" {
-  value = aws_instance.web[*].id
+output "web_launch_template_id" {
+  value = aws_launch_template.web.id
 }
 
-output "app_instance_public_ips" {
-  value = aws_instance.app[*].public_ip
+output "app_asg_name" {
+  value = aws_autoscaling_group.app.name
 }
 
-output "web_instance_public_ips" {
-  value = aws_instance.web[*].public_ip
+output "web_asg_name" {
+  value = aws_autoscaling_group.web.name
 }
